b11aaed8afb42ea29459d407286a9650ee9d1013
MBacioglu <302145958@student.rocmondriaan.nl>
2024-01-20 00:39:59 +0100

    out of bounds systeem toegevoegd

--- js/main.js
+++ js/main.js
@@ -41,15 +41,30 @@ function update () {
     context.fillRect(player.x, player.y, player.width, player.height);
 }
 
+function outOfBounds(xPosition) {
+    return (xPosition < 0 || xPosition + playerWidth > boardWidth);
+    
+}
+
 function movePlayer(e) {
 
     if (e.code == "ArrowLeft") {
-        player.x -= player.velocityX;} 
+        //player.x -= player.velocityX;} 
+        let nextPlayerX = player.x - player.velocityX;
+        if (!outOfBounds(nextPlayerX)) {
+            player.x = nextPlayerX;
+        }
 
-
+    }
     else if (e.code == "ArrowRight") {
-        player.x += playerVelocityX}
-    
+        //player.x += playerVelocityX}
+        let nextPlayerX = player.x + player.velocityX;
+        if (!outOfBounds(nextPlayerX)) {
+            player.x = nextPlayerX;
+        }
+  }
 }
+    
+
 
 
