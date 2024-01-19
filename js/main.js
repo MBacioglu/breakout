6f03f55ec0d08778229408984e8b5bbb254a8d6b
MBacioglu <302145958@student.rocmondriaan.nl>
2024-01-19 23:08:51 +0100

    nieuwe codes toegevoegd en platform geprobeerd te animeren

--- js/main.js
+++ js/main.js
@@ -8,12 +8,14 @@ let context;
 // player 
 let playerHeight = 10;
 let playerWidth = 80;
+let playerVelocityX = 10;
 
 let player = {
     x : boardWidth/2 - playerWidth/2,
     y : boardHeight - playerHeight - 5,
     width : playerWidth,
     height : playerHeight,
+    velocityX : playerVelocityX
 }
 
 window.onload = function () {
@@ -22,8 +24,31 @@ window.onload = function () {
     board.width = boardWidth;
     context = board.getContext("2d"); // dit is om de board te tekenen
 
-    // dit is player
+    // dit is om de player te tekenen
+    context.fillStyle = "greenyellow";
+    context.fillRect(player.x, player.y, player.width, player.height);
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", movePlayer);
+}
+
+function update () {
+    requestAnimationFrame(update);
+
+    //player
     context.fillStyle = "greenyellow";
     context.fillRect(player.x, player.y, player.width, player.height);
 }
+
+function movePlayer(e) {
+
+    if (e.code == "ArrowLeft") {
+        player.x -= player.velocityX;} 
+
+        
+    else if (e.code == "ArrowRight") {
+        player.x += playerVelocityX}
+    
+}
+
 
